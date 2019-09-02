b24db615c3b4c1c433efa08b6999af6f0bb89117
韩剑 <hanjian1106@163.com>
2019-09-02 20:14:36 +0800

    commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# node-floatobj
+## 安装
+
+> use npm install
+>
+> 使用npm安装
+
+```
+npm install node
+```
+
---
+++ test/index.js
@@ -0,0 +1,10 @@
+var floatObj = require('../index');
+console.log(floatObj);
+
+console.log(floatObj.add(1099.891,100.1));
+console.log(floatObj.subtract(1099.891,100.1));
+console.log(floatObj.multiply(1099.89,1000));
+console.log(floatObj.divide(1099.89,100));
+
+
+console.log(1098.89/100);
